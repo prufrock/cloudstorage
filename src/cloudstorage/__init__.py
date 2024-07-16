ef4bf1a916c06008b1245d9b6e430001aa5d8645
David Kanenwisher <d.kanen@gmail.com>
2024-07-15 21:46:22 -0500

    Update __init__.py

--- src/cloudstorage/__init__.py
+++ src/cloudstorage/__init__.py
@@ -20,7 +20,7 @@ __all__ = [
 ]
 
 __title__ = "Cloud Storage"
-__version__ = "0.11.0"
+__version__ = "0.11.0a1"
 __author__ = "Scott Werner"
 __license__ = "MIT"
 __copyright__ = "Copyright 2017-2018 Scott Werner"
